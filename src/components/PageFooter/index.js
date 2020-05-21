b984ca41f81e48cbd1bde5f43c5a2aa7bb313cc3
MutegekiHenry <henrymutegeki117@gmail.com>
2020-05-21 12:35:09 +0300

    feat: add footer component

---
+++ src/components/PageFooter/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PageFooter.css';
+
+const LandingFooter = () => (
+  <footer className="LandingFooter">
+    <p className="LandingFooterCopyright">
+      Copyright © 2020 Slam Data. All Rights Reserved.
+    </p>
+  </footer>
+);
+
+export default LandingFooter;
