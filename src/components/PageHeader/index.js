319fe3f71b09f88a6149311edf7f8c30ab523f65
MutegekiHenry <henrymutegeki117@gmail.com>
2020-05-21 12:31:12 +0300

    feat: add page header component

---
+++ src/components/PageHeader/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../images/logo.png';
+import './PageHeader.css';
+
+const Header = () => (
+  <header className="Header">
+    <div>
+      <Link to="/" className="LogoLink"> <img src={logo} alt="" /></Link>
+    </div>
+    <div className="HeaderLinksWrap">
+      <div className="HeaderLinks bold uppercase">
+        <Link to="#" className="HeaderLinkPricing">pricing</Link>
+        <Link to="#" className="HeaderLinkDocs">docs</Link>
+        <Link to="/login" className="HeaderLinkLogin">login</Link>
+      </div>
+    </div>
+  </header>
+);
+
+export default Header;
